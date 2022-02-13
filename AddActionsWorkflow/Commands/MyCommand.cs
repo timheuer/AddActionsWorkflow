e3688fe8eee86fce1386ec3bcde7c8275a52d961
Tim Heuer <tim@timheuer.com>
2022-02-12 17:21:46 -0800

    Add project files.

---
+++ AddActionsWorkflow/Commands/MyCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace AddActionsWorkflow;
+
+[Command(PackageIds.MyCommand)]
+internal sealed class MyCommand : BaseCommand<MyCommand>
+{
+    protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+    {
+        var dirInfo = new DirectoryInfo(VS.Solutions.GetCurrentSolution().FullPath);
+
+        var proc = new ProcessStartInfo();
+        proc.UseShellExecute = false;
+        proc.RedirectStandardOutput = true;
+        proc.RedirectStandardError = true;
+        proc.WorkingDirectory = dirInfo.Parent.FullName;
+        proc.FileName = "dotnet";
+        proc.Arguments = "new workflow -n build --no-update-check";
+
+        Process.Start(proc);
+    }
+}
